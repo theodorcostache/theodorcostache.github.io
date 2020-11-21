d5097b17e88ee7d3b69bf38f8dfe83c4d06cb3e0
Theodor Costache <costache.theodor@gmail.com>
2020-11-21 21:55:45 +0100

    deployment

---
+++ precache-manifest.469477fae59cd3f29c036fddf1aa36c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfc6a3253817ed5f7b99b787e1fb7d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3a97197dff8952bf135",
+    "url": "/static/css/2.7c3fc33d.chunk.css"
+  },
+  {
+    "revision": "f1d3deda222fe394fe5b",
+    "url": "/static/css/main.dd5fd022.chunk.css"
+  },
+  {
+    "revision": "a3a97197dff8952bf135",
+    "url": "/static/js/2.45b89e4a.chunk.js"
+  },
+  {
+    "revision": "94360fa49df6b85ef450b972e8b68ef8",
+    "url": "/static/js/2.45b89e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d3deda222fe394fe5b",
+    "url": "/static/js/main.6ce5c53f.chunk.js"
+  },
+  {
+    "revision": "95f7177625900ac2ce03",
+    "url": "/static/js/runtime-main.254145be.js"
+  }
+]);
