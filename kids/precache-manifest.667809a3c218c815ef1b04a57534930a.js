3ff7d78f7d2095330c48ec1cb7796f7275b83580
Theodor Costache <costache.theodor@gmail.com>
2020-11-21 21:25:24 +0100

    deployment

---
+++ kids/precache-manifest.667809a3c218c815ef1b04a57534930a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b282fbddf678725ae38fb586589890",
+    "url": "/kids/index.html"
+  },
+  {
+    "revision": "3ff2f86dcb3754c80e13",
+    "url": "/kids/static/css/main.68852bd4.chunk.css"
+  },
+  {
+    "revision": "1932e6b1df488e066623",
+    "url": "/kids/static/js/2.5fc2a04e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/kids/static/js/2.5fc2a04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ff2f86dcb3754c80e13",
+    "url": "/kids/static/js/main.1bb32933.chunk.js"
+  },
+  {
+    "revision": "9068fe41c505c8f8321a",
+    "url": "/kids/static/js/runtime-main.bac45b93.js"
+  }
+]);
